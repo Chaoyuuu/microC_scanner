cdea193d2d46e328fd699406efe3b35b2301edf9
Chaoyuuu <a0935292660@gmail.com>
2019-04-17 00:57:57 +0800

    push

---
+++ input/testjadzzz.c
@@ -0,0 +1,6 @@
+// this is a comment // line */ /* with /* delimiters */ before the end
+/* this is a comment // line with some /* and C delimiters */
+//string a = "h\"a"
+"\"\"\"hhhhh\""
+
+
---
+++ test.c
@@ -0,0 +1,10 @@
+int a = [1, 2, 3]
+int b = -123
+string c = "hello\""
+/*
+* This is a comment
+*/
+
+/* Another comment */
+
+// Thrid comment
